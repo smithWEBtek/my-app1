80f9b505d7cfafe4b452488cf1926e63c57694e9
smithwebtek <brad@smithwebtek.com>
2017-11-01 20:05:50 -0400

    initial upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to My Almost First React App</h1>
+        </header>
+        <p className="App-intro">
+          <p>To get started, edit <code>src/App.js</code> and save to reload.</p>
+          Wow, I won't ever have to refresh a web page again.
+          
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
